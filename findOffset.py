d154ceae6237cec82df834b0eeba7400510e28f1
ArchCWithClasses <86631513+ArchCWithClasses@users.noreply.github.com>
2021-10-31 15:04:28 -0500

    Update findOffset.py

--- findOffset.py
+++ findOffset.py
@@ -9,7 +9,7 @@ pattern = "Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2
 try:
 
 		s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-		s.connect(('Machine IP', 9999))
+		s.connect(("Machine IP", 9999))
 		s.send(pattern + "\r\n")
 		s.close()
 	
